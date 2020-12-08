8f44cb3ff13e6c1268ea64799214392943c541ee
Shashank Korthiwada <skorthiwada@manh.com>
2020-12-08 20:21:12 +0530

    style : added css properties

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const button = document.querySelector("#btn-translate");
 const inputText = document.querySelector("#text-area");
 const outputDiv = document.querySelector(".output-div");
+const errorDiv = document.querySelector(".error");
 
-var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-//var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+//var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
 button.addEventListener("click", () => {
-  //outputDiv.innerText = inputText.value;
-
   fetch(serverUrl + "?" + "text=" + inputText.value)
     .then((response) => response.json())
     .then((json) => (outputDiv.innerText = json.contents.translated))
